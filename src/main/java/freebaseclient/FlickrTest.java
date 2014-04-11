1bbe326d35c9f3870648d6af95754693c423f588
tobi <trezmann@gmail.com>
2014-04-11 14:38:53 +0200

    minor changes

--- src/main/java/freebaseclient/FlickrTest.java
+++ src/main/java/freebaseclient/FlickrTest.java
@@ -33,7 +33,6 @@ public class FlickrTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		List<FlickrPhoto> photoTest = new ArrayList<FlickrPhoto>();
 		// photoTest = flickrTest.getPhotosByQuery(query);
 		// photoTest = flickrTest.getPhotosByQueryAndPlace(query, placeName);
