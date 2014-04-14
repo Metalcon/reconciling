f0fb4fa7ef527b34e9346063783d1bf7309c8086
--local <--local>
2014-04-14 16:40:17 +0200

    continued preparing structure

--- src/main/java/albumDetails/AlbumDetailsAPI.java
+++ src/main/java/albumDetails/AlbumDetailsAPI.java
@@ -1,5 +1,6 @@
 package albumDetails;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lastFMAlbum.Album;
@@ -7,7 +8,17 @@ import lastFMAlbum.Album;
 public class AlbumDetailsAPI {
 
 	public List<Album> getAlbums(String bandMid) {
-		// TODO:
+
+		// TODO: get album-mids via band-mids
+		List<Album> albums = new ArrayList<Album>();
+		albums = FreebaseAlbumSearch.getAlbumMids(bandMid);
+
+		// TODO: get primary-release-mids to alum-mids
+
+		// TODO: get lastfm details to primary-release and store them as an
+		// Album List
+
+		// TODO: return Album List.
 		return null;
 	}
 }
