c1056900f02cbd7ba7f2c96f6a96e94fee901edb
--local <--local>
2014-04-14 16:20:46 +0200

    prepared API structure for album detail search

--- src/main/java/albumDetails/AlbumDetailsAPI.java
+++ src/main/java/albumDetails/AlbumDetailsAPI.java
@@ -1,5 +1,13 @@
 package albumDetails;
 
+import java.util.List;
+
+import lastFMAlbum.Album;
+
 public class AlbumDetailsAPI {
 
+	public List<Album> getAlbums(String bandMid) {
+		// TODO:
+		return null;
+	}
 }
