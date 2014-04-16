fe9669baafd0adbdcf98c9dcec2f1000bf5980d0
tobi <trezmann@gmail.com>
2014-04-16 15:20:59 +0200

    added some more testmbids to easy test some more cases

--- src/main/java/albumDetails/TestAlbumDetailsAPI.java
+++ src/main/java/albumDetails/TestAlbumDetailsAPI.java
@@ -10,7 +10,12 @@ public class TestAlbumDetailsAPI {
 	public static void main(String[] args) {
 		List<Album> output = new ArrayList<Album>();
 		AlbumDetailsAPI testApi = new AlbumDetailsAPI();
-		output = testApi.getAlbums("/m/04rcr");
+		String ensiferum = "/m/03y2lh";
+		String metallica = "/m/04rcr";
+		String wintersun = "/m/04473z";
+		String inflames = "/m/0370w8";
+		String amonamarth = "/m/03v5x0";
+		output = testApi.getAlbums(wintersun);
 		for (int i = 0; i < output.size(); i++) {
 			System.out.println(output.get(i).toString());
 		}
